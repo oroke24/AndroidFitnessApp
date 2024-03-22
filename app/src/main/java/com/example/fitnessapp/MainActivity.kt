4fe57ff6b13d607d12d0603164da032a9071b97e
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-03-21 20:01:43 -0700

    Added image to main page.  Added Date button (toast to main page).  cleaned up recipe and exercise activities.

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.fitnessapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,12 +14,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fitnessapp.ui.theme.FitnessAppTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val dateButton = findViewById<Button>(R.id.dateButton)
         val recipeButton = findViewById<Button>(R.id.recipeButton)
         val exerciseButton = findViewById<Button>(R.id.exerciseButton)
 
@@ -30,6 +35,11 @@ class MainActivity : ComponentActivity() {
             val intent = Intent(this, ExerciseActivity::class.java)
             startActivity(intent)
         }
-
+        dateButton.setOnClickListener{
+            val calendar = Calendar.getInstance()
+            val dateFormat = SimpleDateFormat("MM-dd-yyyy", Locale.getDefault())
+            val currentDateFormatted = dateFormat.format(calendar.time)
+            Toast.makeText(this, "Today's date: $currentDateFormatted", Toast.LENGTH_SHORT).show()
+        }
     }
 }
