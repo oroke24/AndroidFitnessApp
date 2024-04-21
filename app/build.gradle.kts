bc36f1a6a57686437d60ccf835d1f9facb5eaafe
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2024-04-21 12:42:46 -0700

    Version 1.2 (versionCode 3) development begin. Todo: make items shareable between users.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.fitnessapp"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -27,7 +27,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            isDebuggable = false
+            isDebuggable = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
